a25c759196024b45c35aa8a03e425816098a6060
Luis Angel <luis.angel.osorio@outlook.com>
2019-11-12 10:17:49 -0600

    Cambio de URL_SERVICIOS

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -3,7 +3,7 @@ export const CLINICA = 1;
 export const HOSPITAL_BASICO = 2;
 export const HOSPITAL_PREMIUM = 3;
 
-export const URL_SERVICIOS = 'http://167.99.224.27';
+export const URL_SERVICIOS = 'https://api.clinitec.com.mx';
 export const GRANT_TYPE = 'password';
 export const CLIENT_ID = '1';
 export const DESTINO = 'Admin';
